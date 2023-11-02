1f28fa89e8e1e2a9d81e707f7df46df06f51b19a
Hashiriky <148580851+Hashiriky@users.noreply.github.com>
2023-11-01 21:32:31 -0300

    Add files via upload

---
+++ exemplo01.java
@@ -0,0 +1,26 @@
+public class exemplo01 {
+	public static void main(String[] args) {
+		Double N1, N2, N3, N4, RES;
+		System.out.println("Digite o primeiro número: ");
+		N1 = Double.parseDouble(System.console().readLine());
+		System.out.println("Digite o segundo número: ");
+		N2 = Double.parseDouble(System.console().readLine());
+		System.out.println("Digite o terceiro número: ");
+		N3 = Double.parseDouble(System.console().readLine());
+		System.out.println("Digite o quarto número: ");
+		N4 = Double.parseDouble(System.console().readLine());
+		
+		//calculadora
+		RES = (N1 + N2 + N3 + N4) / 4;
+		
+        System.out.println(RES);	
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
