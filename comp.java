1d18befa3010a71b80caa7ae47fdd6486c1c5c39
Hashiriky <148580851+Hashiriky@users.noreply.github.com>
2023-11-03 19:36:00 -0300

    Add files via upload
    Verificar se numero é permitido

---
+++ comp.java
@@ -0,0 +1,19 @@
+public class comp {
+
+public static void main(String[] args){
+
+int n1;
+
+System.out.print("Informe um número");
+n1 = Integer.parseInt(System.console().readLine());
+
+if (n1 >= 1  && n1 < 9)
+    System.out.println("Número permitido");
+
+
+if (n1 > 9 ){
+    System.out.printf("Número maior do que permitido");
+    }
+  }
+
+}
